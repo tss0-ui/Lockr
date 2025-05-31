956e06fe1441b4493e1fa59e12b2f2057a5267c4
tss0-ui <travisscottsmith0@gmail.com>
2025-05-31 11:41:07 -0400

    Update vaultService.ts

--- API/src/vault/services/vaultService.ts
+++ API/src/vault/services/vaultService.ts
@@ -1,22 +1,31 @@
-import VaultItem from "../models/VaultItem";
-import { encryptVaultItem, decryptVaultItem } from "../utils/vaultUtils";
+import prisma from "../lib/prisma";
+import { encryptVaultItem, decryptVaultItem } from "../utils/encryption";
 
-export const createItem = async (userId: string, data: any) => {
-  const encrypted = await encryptVaultItem(data);
-  const item = new VaultItem({ userId, ...encrypted });
-  return await item.save();
+export const createVaultItem = async (userId: string, data: any) => {
+  const encrypted = await encryptVaultItem(data, userId);
+  return await prisma.vaultItem.create({
+    data: {
+      userId,
+      ...encrypted,
+    },
+  });
 };
 
-export const getItems = async (userId: string) => {
-  const items = await VaultItem.find({ userId });
-  return items.map(item => decryptVaultItem(item));
+export const getVaultItems = async (userId: string) => {
+  const items = await prisma.vaultItem.findMany({ where: { userId } });
+  return Promise.all(items.map((item) => decryptVaultItem(item)));
 };
 
-export const updateItem = async (userId: string, id: string, data: any) => {
-  const encrypted = await encryptVaultItem(data);
-  return await VaultItem.findOneAndUpdate({ _id: id, userId }, encrypted, { new: true });
+export const updateVaultItem = async (userId: string, itemId: string, data: any) => {
+  const encrypted = await encryptVaultItem(data, userId);
+  return await prisma.vaultItem.update({
+    where: { id: itemId, userId },
+    data: encrypted,
+  });
 };
 
-export const deleteItem = async (userId: string, id: string) => {
-  return await VaultItem.findOneAndDelete({ _id: id, userId });
+export const deleteVaultItem = async (userId: string, itemId: string) => {
+  return await prisma.vaultItem.delete({
+    where: { id: itemId, userId },
+  });
 };
